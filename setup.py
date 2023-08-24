d59d46f13c81b1ff539eb54bcf01f16ad8e9cd40
mukhriddin7585 <mr.muhriddin.20@gmail.com>
2023-08-24 12:46:21 +0500

    setup.py to'g'irlandi

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='Ermit',
-    packages=find_packages(include=['ermit']),
     version='0.1.0',
-    description='Ermit splayn',
+    description='Ermit spline',
     author='Mukhriddin Abduganiev',
+    author_email="mr.muhriddin.20@gmail.com",
+    url="https://github.com/mukhriddin7585/Ermit",
     license='MIT',
-    install_requires=['matplotlib==3.8.0rc1', 'numpy==1.25.2'],
+    python_requires='>3.11.4',
+    packages=['ermit'],
+    install_requires=[
+        'matplotlib==3.8.0rc1',
+        'numpy==1.25.2'
+    ],
     setup_requires=['pytest-runner==6.0.0'],
     tests_require=['pytest==7.4.0'],
     test_suite='tests',
